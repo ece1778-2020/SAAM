66d021580028ee368a8e629e826d6e1f5449553e
Xiaoyi Wang <xiaoyiwang@Xiaoyis-MacBook-Pro.local>
2020-02-21 14:06:07 -0500

    first commit of the project

---
+++ SAAMClient/SAAMClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SAAMClient
+//
+//  Created by Xiaoyi Wang on 2020-02-21.
+//  Copyright © 2020 SAAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
