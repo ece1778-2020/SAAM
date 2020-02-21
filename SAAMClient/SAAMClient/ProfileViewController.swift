1cea065930a45af6327b80e1fc28a4d7ab61b12d
HenryCloud <18811526516@163.com>
2020-02-21 15:53:06 -0500

    podfile

---
+++ SAAMClient/SAAMClient/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  SAAMClient
+//
+//  Created by AdamLi on 2020/2/21.
+//  Copyright © 2020 SAAM. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
